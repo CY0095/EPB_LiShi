369f901c286ec5d93b5f7a730c8056d8b1a98a50
CY0095 <1158524018@qq.com>
2016-07-13 20:45:31 +0800

    提交了一个初始工程

---
+++ EPB_LiShi/Classes/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EPB_LiShi
+//
+//  Created by lanou3g on 16/7/13.
+//  Copyright © 2016年 张鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
